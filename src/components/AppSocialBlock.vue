6feedc6a76527abb859a148a6b8717e2003f3b8a
i-kostenevich <kostenevich9696@gmail.com>
2021-04-08 02:36:02 +0300

    refactoring social list

--- src/components/AppSocialBlock.vue
+++ src/components/AppSocialBlock.vue
@@ -1,5 +1,17 @@
 <template>
   <ul class="social-list">
+    <li v-for="item in socialList" :key="item">
+      <a :href="item.linkTo" @mouseenter="item.isMouseEnter = true" @mouseleave="item.isMouseEnter = false">
+        <inline-svg
+          :src="item.isMouseEnter ? item.iconHover : item.iconImg"
+          width="32"
+          height="32"
+          fill="white"
+        ></inline-svg>
+      </a>
+    </li>
+  </ul>
+  <!-- <ul class="social-list">
     <li><a href="https://t.me/I_kostenevich" target="_blank"></a></li>
     <li><a href="https://wa.me/380678684657" target="_blank"></a></li>
     <li>
@@ -12,18 +24,80 @@
     <li>
       <a href="https://github.com/igor-kostenevich" target="_blank"></a>
     </li>
-  </ul>
+    <li>
+      <a href="https://github.com/igor-kostenevich" target="_blank">
+        <inline-svg
+          :src="require('../assets/images/icons/social/skype.svg')"
+          width="32"
+          height="32"
+          fill="white"
+        ></inline-svg>
+      </a>
+    </li>
+  </ul> -->
 </template>
 
 <script>
-export default {}
+import InlineSvg from 'vue-inline-svg'
+
+export default {
+  data() {
+    return {
+      socialList: [
+        {
+          linkTo: 'https://t.me/I_kostenevich',
+          iconImg: require('../assets/images/icons/social/telegram.svg'),
+          iconHover: require('../assets/images/icons/social/telegram_hover.svg'),
+          isMouseEnter: false,
+        },
+        {
+          linkTo: 'https://wa.me/380678684657',
+          iconImg: require('../assets/images/icons/social/whatsapp.svg'),
+          iconHover: require('../assets/images/icons/social/whatsapp_hover.svg'),
+          isMouseEnter: false,
+        },
+        {
+          linkTo: 'https://join.skype.com/invite/gpJbSHSIqKb1',
+          iconImg: require('../assets/images/icons/social/skype.svg'),
+          iconHover: require('../assets/images/icons/social/skype_hover.svg'),
+          isMouseEnter: false,
+        },
+        {
+          linkTo: 'mailto:kostenevich9696@gmail.com',
+          iconImg: require('../assets/images/icons/social/gmail.svg'),
+          iconHover: require('../assets/images/icons/social/gmail_hover.svg'),
+          isMouseEnter: false,
+        },
+        {
+          linkTo: 'https://www.linkedin.com/in/igor-kostenevich/',
+          iconImg: require('../assets/images/icons/social/linkedin.svg'),
+          iconHover: require('../assets/images/icons/social/linkedin_hover.svg'),
+          isMouseEnter: false,
+        },
+        {
+          linkTo: 'https://github.com/igor-kostenevich',
+          iconImg: require('../assets/images/icons/social/github.svg'),
+          iconHover: require('../assets/images/icons/social/github_hover.svg'),
+          isMouseEnter: false,
+
+        },
+      ],
+    }
+  },
+  methods: {
+    onMouseEnter(item) {
+      item.isMouseEnter = true
+    },
+  },
+  components: { InlineSvg },
+}
 </script>
 
 <style scoped lang="scss">
 .social-list {
   display: flex;
 
-  @media (max-width: 768px){
+  @media (max-width: 768px) {
     justify-content: center;
   }
 
@@ -45,75 +119,66 @@ export default {}
       height: inherit;
     }
 
-    &:nth-child(1) {
-      background: url('../assets/images/icons/social/telegram.svg') center /
-        cover no-repeat;
-      border-radius: 50%;
-      border: 2px solid #fff;
+    // &:nth-child(1) {
+    //   background: url('../assets/images/icons/social/telegram.svg') center /
+    //     cover no-repeat;
+    //   border-radius: 50%;
+    //   border: 2px solid #fff;
 
-      &:hover {
-        background: url('../assets/images/icons/social/telegram_hover.svg')
-          center / cover no-repeat;
-        border: 0;
-        transform: scale(0.88);
-      }
-    }
-    &:nth-child(2) {
-      background: url('../assets/images/icons/social/whatsapp.svg') center /
-        cover no-repeat;
+    //   &:hover {
+    //     background: url('../assets/images/icons/social/telegram_hover.svg')
+    //       center / cover no-repeat;
+    //     border: 0;
+    //     transform: scale(0.88);
+    //   }
+    // }
+    // &:nth-child(2) {
+    //   background: url('../assets/images/icons/social/whatsapp.svg') center /
+    //     cover no-repeat;
 
-      &:hover {
-        background: url('../assets/images/icons/social/whatsapp_hover.svg')
-          center / cover no-repeat;
-      }
-    }
-    &:nth-child(3) {
-      background: url('../assets/images/icons/social/skype.svg') center / cover
-        no-repeat;
+    //   &:hover {
+    //     background: url('../assets/images/icons/social/whatsapp_hover.svg')
+    //       center / cover no-repeat;
+    //   }
+    // }
+    // &:nth-child(3) {
+    //   background: url('../assets/images/icons/social/skype.svg') center / cover
+    //     no-repeat;
 
-      &:hover {
-        background: url('../assets/images/icons/social/skype_hover.svg') center /
-          cover no-repeat;
-      }
-    }
-    &:nth-child(4) {
-      background: url('../assets/images/icons/social/gmail.svg') center / cover
-        no-repeat;
-      width: 40px;
+    //   &:hover {
+    //     background: url('../assets/images/icons/social/skype_hover.svg') center /
+    //       cover no-repeat;
+    //   }
+    // }
+    // &:nth-child(4) {
+    //   background: url('../assets/images/icons/social/gmail.svg') center / cover
+    //     no-repeat;
+    //   width: 40px;
 
-      &:hover {
-        background: url('../assets/images/icons/social/gmail_hover.svg') center /
-          cover no-repeat;
-      }
-    }
-    &:nth-child(5) {
-      background: url('../assets/images/icons/social/linkedin.svg') center /
-        cover no-repeat;
+    //   &:hover {
+    //     background: url('../assets/images/icons/social/gmail_hover.svg') center /
+    //       cover no-repeat;
+    //   }
+    // }
+    // &:nth-child(5) {
+    //   background: url('../assets/images/icons/social/linkedin.svg') center /
+    //     cover no-repeat;
 
-      &:hover {
-        background: url('../assets/images/icons/social/linkedin_hover.svg')
-          center / cover no-repeat;
-      }
-    }
-    &:nth-child(6) {
-      background: url('../assets/images/icons/social/github.svg') center /
-        cover no-repeat;
+    //   &:hover {
+    //     background: url('../assets/images/icons/social/linkedin_hover.svg')
+    //       center / cover no-repeat;
+    //   }
+    // }
+    // &:nth-child(6) {
+    //   background: url('../assets/images/icons/social/github.svg') center /
+    //     cover no-repeat;
 
-      &:hover {
-        background: url('../assets/images/icons/social/github_hover.svg')
-          center / cover no-repeat;
-          transition: .2s;
-      }
-    }
-
-    @media (max-width: 480px){
-      width: 22px;
-      height: 22px;
-
-      &:nth-child(4){
-        width: 28px;
-      }
-    }
+    //   &:hover {
+    //     background: url('../assets/images/icons/social/github_hover.svg')
+    //       center / cover no-repeat;
+    //       transition: .2s;
+    //   }
+    // }
   }
 }
 </style>
